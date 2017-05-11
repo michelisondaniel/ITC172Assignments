ed27ac3d9d2a21f712ba7f36a37a8914cb6653d7
Dan Micheli <michelison.daniel@gmail.com>
2017-05-11 11:58:22 -0700

    Grants1
    First Grant upload

---
+++ WCFAssign/App_Code/GrantService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GrantService" in code, svc and config file together.
+public class GrantService : IGrantService
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+    public List<GrantRequest> GetGrants()
+    {
+        var GrantReq = from g in db.GrantRequests
+                       select g;
+
+        List<GrantRequest> grants = new List<GrantRequest>();
+
+        foreach(var gr in GrantReq)
+            // loop through the request to write each field to the grant request
+        {
+            GrantRequest grants1 = new GrantRequest();
+            grants1.GrantRequestDate = gr.GrantRequestDate;
+            grants1.GrantRequestAmount = gr.GrantRequestAmount;
+            grants1.PersonKey = gr.PersonKey;
+            grants1.GrantTypeKey = gr.GrantTypeKey;
+
+            grants.Add(grants1);
+        }
+        return grants;
+    }
+}
---
+++ WCFAssign/App_Code/IGrantService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGrantService" in both code and config file together.
+[ServiceContract]
+public interface IGrantService
+{
+    [OperationContract]
+    int Login(string user, string password);
+
+    [OperationContract]
+    bool RegisterPerson(PersonInfo pi);
+
+    [OperationContract]
+    List<GrantRequest> GrantRequestByPerson(string PersonLastName);
+
+    [OperationContract]
+    List<GrantReview> GetGrants();
+
+}
+
+[DataContract]
+public class PersonInfo
+{
+    [DataMember]
+    public string lastname { get; set; }
+
+    [DataMember]
+    public string firstname { get; set; }
+
+    [DataMember]
+    public string email { get; set; }
+
+    [DataMember]
+    public string password { get; set; }
+
+    [DataMember]
+    public string ApartmentNumber { get; set;}
+
+    [DataMember]
+    public string Street { get; set; }
+
+    [DataMember]
+    public string City { get; set; }
+
+    [DataMember]
+    public string State { get; set; }
+
+    [DataMember]
+    public string Zipcode { get; set; }
+    [DataMember]
+    public string HomePhone { get; set; }
+
+    [DataMember]
+    public string WorkPhone { get; set; }
+
+}
