ca191882d57694d7e6f84271e3258f903a929a37
Dan Micheli <michelison.daniel@gmail.com>
2017-06-08 12:30:28 -0700

    turning in

---
+++ ApplyGrants.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ApplyGrants : System.Web.UI.Page
+{
+    private decimal amount;
+    private int result;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["userKey"] == null)
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+
+    protected void Submit_Click(object sender, EventArgs e)
+    {
+
+        WCFClient.GrantServiceClient gc = new WCFClient.GrantServiceClient();
+        decimal amount;
+
+        WCFClient.GrantRequest gr = new WCFClient.GrantRequest();
+
+        bool GrantRequestKey = decimal.TryParse(GrantAmountBox.Text, out amount);
+
+        gr.PersonKey = (int)Session["userKey"];
+
+        if (GrantRequestKey)
+        {
+            gr.GrantRequestAmount = amount;
+            gr.GrantRequestDate = DateTime.Now;
+            int? person = null;
+            gr.PersonKey = person;
+
+            Response.Redirect("GrantView.aspx");
+        }
+
+    }
+
+}
+      
---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+        WCFClient.GrantServiceClient sc = new WCFClient.GrantServiceClient();
+
+        int result = sc.Login(UserBox.Text, PasswordBox.Text);
+
+        if (result != 0)
+        {
+            Session["userKey"] = result;
+            Response.Redirect("ApplyGrants.aspx");
+
+        }
+        else
+        {
+            ResultLabel.Text = "Invalid log in";
+        
+        }
+        
+    }
+}
